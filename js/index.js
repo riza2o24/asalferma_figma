83a414ab9a98ad767657794ff77f7ff4a40e485d
Otabek <abdullayevriza80@gmail.com>
2025-12-29 14:11:51 +0500

    wer

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ alert("salom 🖐️")
 
 let yosh=Number(prompt("yoshingizni kiriting"))
 if (yosh >= 18){
-    confirm("siz" + " " + yosh )
+    confirm("siz" + " " + yosh + "yosh misiz")
 }else{
     confirm("siz 18 yosh dan kichik misiz")
 }
